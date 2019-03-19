20f905742c0358682d7c002e978a748b65628ce8
unknown <philibert.alexandre@gmail.com>
2019-03-19 22:02:40 +0100

    Updated Login page margins

--- code/website/scripts/login.js
+++ code/website/scripts/login.js
@@ -2,49 +2,58 @@
 
 document.forms['loginForm'].addEventListener('submit', function(evt) {
 	evt.preventDefault();
-	var email = document.getElementById('email').value;
-	var password = document.getElementById('password').value;
-	
-	if(typeof email === 'undefined') {
-		console.log("email vide");
-		return;
-	}
-	if(typeof password === 'undefined') {
-		console.log("password vide");
-		return;
-	}
+	var emailElem = document.getElementById('email');
+	var passwordElem = document.getElementById('password');
 	
 	var params = {
-		email: email,
-		password: password
+		email: emailElem.value,
+		password: passwordElem.value
 	};
 	
 	login(params).then(function(result) {
-		console.log(result);
+		if(result.status === 200) {
+			setCookie('JWT', result.body.token, 2);
+			window.location.href = 'http://runscape.internet-box.ch/admin';
+			return;
+		}
+		var errorMessageElem = document.getElementById('errorMessage');
+		errorMessage.innerText = result.body;
 	});
 });
 
 function login(params) {
 	return new Promise(function(resolve, reject) {
-		const request = new XMLHttpRequest();
+		var request = new XMLHttpRequest();
 		
-		request.open('GET', 'http://runscape.internet-box.ch/api/token', true);
+		request.open('POST', 'https://runscape.internet-box.ch/api/token', true);
 		request.setRequestHeader('Content-Type', 'application/json');
 		
-		request.onload = function(evt) {
-			if(this.readyState === XMLHttpRequest.DONE) {
-				switch(this.status) {
-					case 200:
-						resolve(req.responseText);
-						break;
-					default:
-						reject(req.responseText);
-						break;
-				}
-			}
+		request.onload = function() {
+			var response = {
+				status: this.status,
+				body: JSON.parse(this.responseText)
+			};
+			resolve(response);
+		}
+		
+		request.ontimeout  = function() {
+			reject();
 		}
 		
 		var body = JSON.stringify(params);
 		request.send(body);
 	});
+}
+
+
+/*
+ *	Create and store a new cookie
+ *
+ *	@days		number of days the cookie stays valid
+ *	@return		true if succeeded, false otherwise
+ *
+*/
+function setCookie(name, value, days) {
+	var date = new Date(Date.now() + 24*60*60*1000*days);
+	document.cookie = name + "=" + value + ";path=/;expires=" + date.toGMTString();
 }
